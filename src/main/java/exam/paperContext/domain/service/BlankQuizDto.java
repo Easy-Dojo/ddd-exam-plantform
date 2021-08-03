b2e63c1d25a39ad08f381d9b9bdde55b663fc18d
Liang, Lingrui <lingrui.liang@capgemini.com>
2021-08-03 23:14:47 +0800

    init

---
+++ src/main/java/exam/paperContext/domain/service/BlankQuizDto.java
@@ -0,0 +1,16 @@
+package exam.paperContext.domain.service;
+
+import exam.paperContext.domain.model.paper.Paper.BlankQuiz;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class BlankQuizDto {
+    private String quizId;
+    private int score;
+
+    public static BlankQuiz toBlankQuiz(BlankQuizDto blankQuizDto) {
+        return new BlankQuiz(blankQuizDto.quizId, blankQuizDto.score);
+    }
+}
